4ff02935552bbd42a06e0d851d190e7ccccb4f2a
Ben Leung <benjamin.leung80@gmail.com>
2019-04-03 07:47:20 +0800

    Add files via upload

--- Multiple Choice Class/sketch.js
+++ Multiple Choice Class/sketch.js
@@ -1,71 +1,101 @@
-var question1;
-var question2;
-var questionX;
-var questionY;
-var max;
 var q1;
 var q2;
-
-var canvasID;
-
-function preload()
-{
-	max = 5;
-
-	questionX = [100,100,100,100,100];
-	questionY = [50,100,150,200,250];
-
-	question1 = new Array(max);
-	question2 = new Array(max);
-
-	question1[0] = loadImage('https://bleungwpg.github.io/p5jsMCandHealthEngine/images/question1_button_answer.png');
-	question1[1] = loadImage('https://bleungwpg.github.io/p5jsMCandHealthEngine/images/question1_button_answer.png');
-	question1[2] = loadImage('https://bleungwpg.github.io/p5jsMCandHealthEngine/images/question1_button_answer.png');
-	question1[3] = loadImage('https://bleungwpg.github.io/p5jsMCandHealthEngine/images/question1_button_answer.png');
-	question1[4] = loadImage('https://bleungwpg.github.io/p5jsMCandHealthEngine/images/question1_button_answer.png');
-
-	question2[0] = loadImage('https://bleungwpg.github.io/p5jsMCandHealthEngine/images/question2_button_answer.png');
-	question2[1] = loadImage('https://bleungwpg.github.io/p5jsMCandHealthEngine/images/question2_button_answer.png');
-	question2[2] = loadImage('https://bleungwpg.github.io/p5jsMCandHealthEngine/images/question2_button_answer.png');
-	question2[3] = loadImage('https://bleungwpg.github.io/p5jsMCandHealthEngine/images/question2_button_answer.png');
-	question2[4] = loadImage('https://bleungwpg.github.io/p5jsMCandHealthEngine/images/question2_button_answer.png');
-
-	q1 = new MultipleChoice(question1,questionX,questionY,0);
-	q2 = new MultipleChoice(question2,questionX,questionY,1);
-
-}
+var q3;
+var questionManager = new QuestionManager(3);
+var questionImage1 = new Array(5);
+var questionImage2 = new Array(5);
+var questionImage3 = new Array(5);
+var currentQuestion = 0;
 
 function setup()
 {
-	createCanvas(800,600);
+	createCanvas(500,500);
 
-	canvasID = 0;
+	setupQuestion1();
+	setupQuestion2();
+	setupQuestion3();
+
+	questionManager.addQuestionData(q1);
+	questionManager.addQuestionData(q2);
+	questionManager.addQuestionData(q3);
+
 }
 
 function draw()
 {
-	background(0,0,0);
+	background(255,255,255);
 
-	if (canvasID == 0)
+	currentQuestion = questionManager.getCurrentQuestionData();
+
+
+	if (currentQuestion.isQuestionAnswered() == 0)
 	{
-		q1.showChoices();
-		if (q1.isButtonClicked() == 1)
-		{
-			canvasID = 1;
-		}
+		currentQuestion.showQuestion();
 	}
-	else if (canvasID == 1)
+	else if (currentQuestion.isQuestionAnswered() == 1)
 	{
-		q2.showChoices();
-		if (q2.isButtonClicked() == 1)
-		{
-			canvasID = 2;
-		}
+		fill(0,255,0);
+		text("Correct",10,10);
+		console.log('correct');
+		questionManager.nextQuestion();
 	}
-	else if (canvasID == 2)
+	else if (currentQuestion.isQuestionAnswered() == -1)
 	{
-		fill(255,255,255);
-		text("Add more code for additional questions here",10,50);
+		fill(255,0,0);
+		text("Incorrect",10,10);
+		questionManager.nextQuestion();
+		console.log('incorrect');
 	}
 
+}
+
+function setupQuestion1()
+{
+	questionImage1[0] = loadImage('https://bleungwpg.github.io/resourcehosting/q1Question.png');
+	questionImage1[1] = loadImage('https://bleungwpg.github.io/resourcehosting/q1Answer1.png');
+	questionImage1[2] = loadImage('https://bleungwpg.github.io/resourcehosting/q1Answer2.png');
+	questionImage1[3] = loadImage('https://bleungwpg.github.io/resourcehosting/q1Answer3.png');
+	questionImage1[4] = loadImage('https://bleungwpg.github.io/resourcehosting/q1Answer4.png');
+
+	q1 = new Question(4);
+	q1.setCorrectAnswer(4,0,50);
+	q1.setQuestion(questionImage1[0]);
+	q1.setAnswer(1,questionImage1[1],50,150,239,40);
+	q1.setAnswer(2,questionImage1[2],50,200,149,40);
+	q1.setAnswer(3,questionImage1[3],50,250,176,40);
+	q1.setAnswer(4,questionImage1[4],50,300,184,40);
+}
+
+function setupQuestion2()
+{
+	questionImage2[0] = loadImage('https://bleungwpg.github.io/resourcehosting/q2Question.png');
+	questionImage2[1] = loadImage('https://bleungwpg.github.io/resourcehosting/q2Answer1.png');
+	questionImage2[2] = loadImage('https://bleungwpg.github.io/resourcehosting/q2Answer2.png');
+	questionImage2[3] = loadImage('https://bleungwpg.github.io/resourcehosting/q2Answer3.png');
+	questionImage2[4] = loadImage('https://bleungwpg.github.io/resourcehosting/q2Answer4.png');
+
+	q2 = new Question(4);
+	q2.setCorrectAnswer(2,0,50);
+	q2.setQuestion(questionImage2[0]);
+	q2.setAnswer(1,questionImage2[1],50,150,239,40);
+	q2.setAnswer(2,questionImage2[2],50,200,149,40);
+	q2.setAnswer(3,questionImage2[3],50,250,176,40);
+	q2.setAnswer(4,questionImage2[4],50,300,184,40);
+}
+
+function setupQuestion3()
+{
+	questionImage3[0] = loadImage('https://bleungwpg.github.io/resourcehosting/q3Question.png');
+	questionImage3[1] = loadImage('https://bleungwpg.github.io/resourcehosting/q3Answer1.png');
+	questionImage3[2] = loadImage('https://bleungwpg.github.io/resourcehosting/q3Answer2.png');
+	questionImage3[3] = loadImage('https://bleungwpg.github.io/resourcehosting/q3Answer3.png');
+	questionImage3[4] = loadImage('https://bleungwpg.github.io/resourcehosting/q3Answer4.png');
+
+	q3 = new Question(4);
+	q3.setCorrectAnswer(2,0,50);
+	q3.setQuestion(questionImage3[0]);
+	q3.setAnswer(1,questionImage3[1],50,150,239,40);
+	q3.setAnswer(2,questionImage3[2],50,200,149,40);
+	q3.setAnswer(3,questionImage3[3],50,250,176,40);
+	q3.setAnswer(4,questionImage3[4],50,300,184,40);
 }
